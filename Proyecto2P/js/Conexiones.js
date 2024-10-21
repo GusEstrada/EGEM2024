b100edad8ee5709e46a45120dd298566df05692c
GusEstrada <151456633+GusEstrada@users.noreply.github.com>
2024-10-21 17:08:05 -0600

    Add files via upload

---
+++ Proyecto2P/js/Conexiones.js
@@ -0,0 +1,20 @@
+function redirectToColecciones() {
+    window.location.href = "Colecciones.html";
+}
+
+function redirectToContactos() {
+    window.location.href = "Contactos.html";
+}
+
+function redirectToHome() {
+    window.location.href = "Home.html";
+}
+
+function redirectToNosotros() {
+    window.location.href = "Nosotros.html";
+}
+
+function redirectToServicios() {
+    window.location.href = "Servicios.html";
+}
+     
